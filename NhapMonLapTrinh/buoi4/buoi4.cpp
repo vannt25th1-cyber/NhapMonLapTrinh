bfce44ea06db35a97497c8559014d15dc9086838
buoi4 <Administrator@PMT2030>
2025-12-04 10:48:49 +0700

    buoi4

--- NhapMonLapTrinh/buoi4/buoi4.cpp
+++ NhapMonLapTrinh/buoi4/buoi4.cpp
@@ -3,22 +3,7 @@
 #include <string>
 #include <cmath>
 using namespace std;
-void cau1() {
-    int namsinh;
-    string hoten;
-    bool gioitinh;
-    cout << "Nhap ho ten: ";
-    getline(cin, hoten);
-    cout << "Nhap nam sinh: ";
-    cin >> namsinh;
-    cout << "Nhap gioi tinh (1- nam; 0- nu): ";
-    int temp;
-    cin >> temp;
-    gioitinh = temp == 1 ? true : false;
-    cout << hoten << "\t" << namsinh << "\t" << (gioitinh == true ? "Nam" : "Nu") << endl;
-
-
-}void cau2() {
+void cau1()   void cau2() {
     int sotien;
     cout << "Nhap so tien: "; 
     cin >> sotien;
