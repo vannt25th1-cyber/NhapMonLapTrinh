47124abc9b26fbf1fb646deadd21830282e34203
Administrator <Administrator@PMT2030>
2025-12-11 13:43:43 +0700

    buoi6

--- NhapMonLapTrinh/buoi6/buoi6.cpp
+++ NhapMonLapTrinh/buoi6/buoi6.cpp
@@ -1,20 +1,19 @@
 
 #include <iostream>
 using namespace std;
+
+void cau1() {
+	int n, tong = 0;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		tong += i;
+	}
+	cout << "tong: " << tong << endl;
+}
+
+     
 int main()
 {
-	for (int i = 1; i <= 5; i++) {
-		cout << "For Hello: " << i << endl;
-	}
-	int j = 1;
-	while (j <= 5) {
-		cout << "While Hello: " << j << endl;
-		j++;
-	}
-	int k = 1;
-	do {
-		cout << "Do..While Hello: " << k << endl;
-		k++;
-	} while (k <= 5);
-
+	cau1();
 }
