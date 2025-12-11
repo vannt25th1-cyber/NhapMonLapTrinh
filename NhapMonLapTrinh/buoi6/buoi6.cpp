3f025f0e9f0c40b3523b4c4ee3ce0d0124ba6287
Administrator <Administrator@PMT2030>
2025-12-11 15:01:07 +0700

    buoi6

--- NhapMonLapTrinh/buoi6/buoi6.cpp
+++ NhapMonLapTrinh/buoi6/buoi6.cpp
@@ -37,7 +37,42 @@ void cau4() {
 		cout << n << "x" << i << " = " << n * i << endl;
 	}
 }
-
+void cau5() {
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			cout << i << endl;
+		}
+	}
+	void cau6() {
+		int n, tong = 0;
+		cout << "Nhap n: ";
+		cin >> n;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				tong += i;
+			}
+		}
+		cout << "Tong cac uoc: " << tong << endl;
+	}
+	void cau7() {
+		int n, tong = 0;
+		cout << "Nhap n: ";
+		cin >> n;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				tong += i;
+			}
+		}
+		if (tong == n)
+			cout << n << "la so hoan thien" << endl;
+		else {
+			cout << n << "khong la so hoan thien" << endl;
+		}
+	}
+		
      
 int main()
 {
